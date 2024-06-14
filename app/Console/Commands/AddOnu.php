6e5b83fa122dd9a86b101e09da0d3188c04231b8
Johan Hoyos <ci83rtda@live.com>
2024-06-14 08:28:17 -0400

    add activation of onu on uisp.

--- app/Console/Commands/AddOnu.php
+++ app/Console/Commands/AddOnu.php
@@ -60,7 +60,7 @@ class AddOnu extends Command
         $clientsAddedServices = $clientsAddedServices->getBody()->getContents();
         $filteredData = collect($clientsAddedServices)->where('servicePlanId',24)->first();
 
-        dd($filteredData);
+        dd($filteredData,$clientsAddedServices);
 
         $sites = [];
         $clientSiteData = [];
