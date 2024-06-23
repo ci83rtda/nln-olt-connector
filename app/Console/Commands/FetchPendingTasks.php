9880c6fde7cf36328ddf7772b3c33f8f73da500a
Johan Hoyos <ci83rtda@live.com>
2024-06-23 03:52:02 -0400

    add check if specific onu is pending to be activated.

--- app/Console/Commands/FetchPendingTasks.php
+++ app/Console/Commands/FetchPendingTasks.php
@@ -35,7 +35,7 @@ class FetchPendingTasks extends Command
             'Authorization' => 'Bearer '. $token,
         ])->get($url);
 
-        Log::info(json_encode($response->json()));
+        Log::info(json_encode($response->reason()));
         if ($response->successful()) {
             $tasks = $response->json();
 
