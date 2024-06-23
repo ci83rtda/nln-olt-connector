337ffa7fbc57c7c72ad2dcae57a4020e1ee9dc74
Johan Hoyos <ci83rtda@live.com>
2024-06-23 04:34:36 -0400

    add check if specific onu is pending to be activated.

--- app/Jobs/GetPendingOnuStatusJob.php
+++ app/Jobs/GetPendingOnuStatusJob.php
@@ -23,7 +23,7 @@ class GetPendingOnuStatusJob extends BaseTaskJob
             $result = $oltConnector->checkActivationSerial($activationSerial);
 
             Log::info("ONU status retrieved ". json_encode($result));
-            $this->reportCompletion(2, $result);
+            $this->reportCompletion($result['"exists'] ? 2 : 5, $result);
         } catch (\Exception $e) {
             Log::error('Error retrieving ONU status: ' . $e->getMessage());
             $this->reportCompletion(4, ['message' => $e->getMessage()]);
