f2c642654b31cee5084dd6c76a38fc6745b6618b
Johan Hoyos <ci83rtda@live.com>
2024-06-24 05:15:21 -0400

    add check if specific onu is pending to be activated.

--- app/Services/OltHelper.php
+++ app/Services/OltHelper.php
@@ -96,7 +96,8 @@ class OltHelper
             $oltConnector->executeCommand("onu $onuId pri wifi_switch 1 enable fcc channel 0 80211bgn 20 20/40", false);
             $oltConnector->executeCommand("onu $onuId pri wifi_ssid 1 name {$params['wifi_ssid']} hide disable auth_mode wpa2psk encrypt_type tkipaes shared_key {$params['shared_key']} rekey_interval 0", false);
         }
-        $oltConnector->executeCommand("onu $onuId pri username admin_control enable {$params['admin_username']} {$params['admin_password']} user_control enable {$params['client_username']} {$params['client_password']}", false);
+        $oltConnector->executeCommand("onu $onuId pri tr069_mng disable", false);
+        $oltConnector->executeCommand("onu $onuId pri username admin_control enable admin {$params['admin_password']} user_control disable user {$params['client_password']}", false);
         $oltConnector->executeCommand("onu $onuId pri firewall level middle", false);
         $oltConnector->executeCommand("onu $onuId pri acl ping control enable lan enable wan enable ipv4_control disable ipv6_control disable", false);
         $oltConnector->executeCommand("onu $onuId pri catv {$params['catv']}", false);
