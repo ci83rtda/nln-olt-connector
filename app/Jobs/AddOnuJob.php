8ad366de56feb30e4e695170f37f54b700eca8f6
Johan Hoyos <ci83rtda@live.com>
2024-06-23 20:47:22 -0400

    add check if specific onu is pending to be activated.

--- app/Jobs/AddOnuJob.php
+++ app/Jobs/AddOnuJob.php
@@ -52,21 +52,14 @@ class AddOnuJob extends BaseTaskJob
             $task['model'] = 'EG8143H5';
             $task['catv'] = $task['hasPendingCatv'] ? 'unlock' : 'lock';
         }
-        $params = [
-            'description' => $task['onuDescription'],
-            'vlanid' => $task['vlanid'],
-            'ip' => $task['ip'],
-            'mask' => $task['subnet'],
-            'gw' => $task['gateway'],
-            'dns_master' => $task['dns1'],
-            'dns_slave' => $task['dns2'],
-            'wifi_ssid' => $task['wifiName'],
-            'shared_key' => $task['wifiPassword'],
-            'modelSelection' => $task['modelSelection'],
-            'model' => $task['model'],
-            'catv' => $task['catv'],
-            'hasPendingCatv' => $task['hasPendingCatv'],
-        ];
+
+        $task['description'] = $task['onuDescription'];
+        $task['mask'] = $task['subnet'];
+        $task['gw'] = $task['gateway'];
+        $task['dns_master'] = $task['dns1'];
+        $task['dns_slave'] = $task['dns2'];
+        $task['wifi_ssid'] = $task['wifiName'];
+        $task['shared_key'] = $task['wifiPassword'];
 
         while (true){
             $deviceUuid = Str::uuid();
@@ -94,7 +87,7 @@ class AddOnuJob extends BaseTaskJob
             "modelName" => $modeloOnu,
             "systemName" => "pi-monitor",
             "vendorName" => $vendorName,
-            "ipAddress" => $params['ip'],
+            "ipAddress" => $task['ip'],
             "macAddress" => $macAddress,
             "deviceRole" => "router",
             "siteId" => $siteId,
@@ -113,7 +106,7 @@ class AddOnuJob extends BaseTaskJob
                     "name" => "eth1",
                     "mac" => $macAddress,
                     "type" => "eth",
-                    "addresses" => [$params['ip'].'/'.$task['subnetCidr']]
+                    "addresses" => [$task['ip'].'/'.$task['subnetCidr']]
                 ]
             ]
         ];
@@ -126,22 +119,22 @@ class AddOnuJob extends BaseTaskJob
 
         if (in_array($task['catvServiceStatus'],[0,4,7,8]) && $task['activateCatv']){
             UispV1Access::doRequest("clients/services/{$task['catvServiceId']}/activate-quoted",'PATCH',[
-                'activeFrom' => now()->format('Y-m-d\TH:i:sO'),
-                'invoicingStart' => now()->format('Y-m-d\TH:i:sO')
+                'activeFrom' => now()->timezone('America/Bogota')->format('Y-m-d\TH:i:sO'),
+                'invoicingStart' => now()->timezone('America/Bogota')->format('Y-m-d\TH:i:sO')
             ]);
         }
 
         if (in_array($task['serviceStatus'],[0,4,7,8])){
             UispV1Access::doRequest("clients/services/{$task['serviceId']}/activate-quoted",'PATCH',[
-                'activeFrom' => now()->format('Y-m-d\TH:i:sO'),
-                'invoicingStart' => now()->format('Y-m-d\TH:i:sO')
+                'activeFrom' => now()->timezone('America/Bogota')->format('Y-m-d\TH:i:sO'),
+                'invoicingStart' => now()->timezone('America/Bogota')->format('Y-m-d\TH:i:sO')
             ]);
         }
 
 
         try {
             Log::info('Attempting to connect to OLT');
-            $oltConnector->addOnu($port, $serialNumber, $params);
+            $oltConnector->addOnu($port, $serialNumber, $task);
             Log::info('Connected to OLT, adding ONU');
         } catch (\Exception $e) {
             Log::error('Error adding ONU: ' . $e->getMessage());
