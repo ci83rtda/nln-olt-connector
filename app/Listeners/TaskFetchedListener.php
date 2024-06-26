b23395e00d0a312a11634a0ba153e7764dbe93df
Johan Hoyos <ci83rtda@live.com>
2024-06-25 23:18:22 -0400

    add check if specific onu is pending to be activated.

--- app/Listeners/TaskFetchedListener.php
+++ app/Listeners/TaskFetchedListener.php
@@ -23,27 +23,27 @@ class TaskFetchedListener
         switch ($task['task_type']) {
             case 3:
                 Log::info("Dispatching ToggleCatvStatusJob for task: " . $task['id']);
-                ToggleCatvStatusJob::dispatch($task);
+                ToggleCatvStatusJob::dispatch($task)->onQueue('tasks');
                 break;
             case 2:
                 Log::info("Dispatching AddOnuJob for task: " . $task['id']);
-                AddOnuJob::dispatch($task);
+                AddOnuJob::dispatch($task)->onQueue('tasks');
                 break;
             case 4:
                 Log::info("Dispatching ChangeWifiSettingsJob for task: " . $task['id']);
-                ChangeWifiSettingsJob::dispatch($task);
+                ChangeWifiSettingsJob::dispatch($task)->onQueue('tasks');
                 break;
             case 5:
                 Log::info("Dispatching GetWifiDetailsJob for task: " . $task['id']);
-                GetWifiDetailsJob::dispatch($task);
+                GetWifiDetailsJob::dispatch($task)->onQueue('tasks');
                 break;
             case 6:
                 Log::info("Dispatching GetOnuStatusJob for task: " . $task['id']);
-                GetOnuStatusJob::dispatch($task);
+                GetOnuStatusJob::dispatch($task)->onQueue('tasks');
                 break;
             case 7:
                 Log::info("Dispatching Check ONU status for task: " . $task['id']);
-                GetPendingOnuStatusJob::dispatch($task);
+                GetPendingOnuStatusJob::dispatch($task)->onQueue('tasks');
                 break;
             default:
                 Log::error('Unknown task action.');
